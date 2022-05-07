28289d9bbc44e730dd81a3dc206f1fde2689211b
aestanley49 <aestanley49@gmail.com>
2022-05-07 17:45:48 -0400

    Adding tutorials for SAs

--- SimAnlTutorialWorkspace.R
+++ SimAnlTutorialWorkspace.R
@@ -13,17 +13,351 @@
 
 ### try working through the following (might have to do this without package..) 
 
+#### These are both in python 
 ## https://towardsdatascience.com/optimization-techniques-simulated-annealing-d6a4785a1de7
 ### https://towardsdatascience.com/simulated-annealing-for-clustering-problems-part-1-3fa8994a3ebb
 
 
-### XX
+
+### published info with GenSA package
+# https://journal.r-project.org/archive/2013/RJ-2013-002/RJ-2013-002.pdf
+# information for how to use GenSA package
+
+# Example down on page 9 on risk 
+
+
+library("quantmod")
+tickers <- c("GE", "IBM", "JPM", "MSFT", "WMT")
+getSymbols(tickers, from = "2000-12-01", to = "2010-12-31")
+#[1] "GE" "IBM" "JPM" "MSFT" "WMT"
+P <- NULL
+for(ticker in tickers) {
+  tmp <- Cl(to.monthly(eval(parse(text = ticker))))
+  P <- cbind(P, tmp)
+  }
+colnames(P) <- tickers
+R <- diff(log(P))
+R <- R[-1,]
+
+mu <- colMeans(R)
+sigma <- cov(R)
+library("PerformanceAnalytics")
+pContribCVaR <- ES(weights = rep(0.2, 5),
+                     method = "gaussian", portfolio_method = "component",
+                     mu = mu, sigma = sigma)$pct_contrib_ES
+obj <- function(w) {
+  fn.call <<- fn.call + 1
+  if (sum(w) == 0) { w <- w + 1e-2 }
+  w <- w / sum(w)
+  CVaR <- ES(weights = w,
+               method = "gaussian", portfolio_method = "component",
+               mu = mu, sigma = sigma)
+  tmp1 <- CVaR$ES
+  tmp2 <- max(CVaR$pct_contrib_ES - 0.225, 0)
+  out <- tmp1 + 1e3 * tmp2
+  return(out)
+}
+
+set.seed(1234)
+fn.call <<- 0
+out.GenSA <- GenSA(fn = obj, lower = rep(0, 5), upper = rep(1, 5),
+                     control = list(smooth = FALSE, max.call = 3000))
+
+fn.call.GenSA <- fn.call
+out.GenSA$value
+##[1] 0.1141484884
+out.GenSA$counts
+##[1] 3000
+cat("GenSA call functions", fn.call.GenSA, "times.\n")
+### GenSA call functions 3000 times.
+wstar.GenSA <- out.GenSA$par
+wstar.GenSA <- wstar.GenSA / sum(wstar.GenSA)
+rbind(tickers, round(100 * wstar.GenSA, 2))
+### [,1] [,2] [,3] [,4] [,5]
+### tickers "GE" "IBM" "JPM" "MSFT" "WMT"
+### "18.92" "21.23" "8.33" "15.92" "35.6"
+100 * (sum(wstar.GenSA * mu) - mean(mu))
+## [1] 0.03790568876
+
+
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+
+
+
+
+
+
+
+### circle weighted.. see how set up SA in code
 # https://jmsallan.netlify.app/blog/coding-simulated-annealing-in-r/
+
+
+# function returns the distance to the origin of the center of mass of a possible solution
+mass_center <- function(v){
+  n <- length(v)
+  
+  angles <- 0:(n-1) * 2 * pi / n
+  x <- v*cos(angles)
+  y <- v*sin(angles)
+  dcm <- c(sum(x), sum(y))/sum(v)
+  dcm <- sqrt(sum(dcm^2))
+  
+  return(dcm)
+}
+
+## How to test different solutions (swap them..?)
+swap <- function(v, i , j){
+  aux <- v[i]
+  v[i] <- v[j]
+  v[j] <- aux
+  return(v)
+}
+
+
+
+## Now setting up SA... 
+
+sa_circle <- function(inisol, iter=1000, alpha = 0.9, p0 = 0.9, eval=FALSE){
+  
+  #setting up tracking of evolution if eval=TRUE
+  if(eval){
+    evalfit <- numeric()
+    evalbest <- numeric()
+    temp <- numeric()
+  }
+  
+  n <- length(inisol)
+  count <- 1
+  
+  #initialization of explored solution sol and best solution bestsol
+  #and objective funciton values fit  and bestfit
+  sol <- inisol
+  bestsol <- inisol
+  fit <- mass_center(sol) ## run through function to find mass center for that solution
+  bestfit <- fit
+  
+  #estimation of initial temperature
+  sol_a <- sapply(1:100, function(x) mass_center(c(1, sample(2:n, n-1))))
+  sol_b <- sapply(1:100, function(x) mass_center(c(1, sample(2:n, n-1))))
+  delta_f0 <- mean(abs(sol_a - sol_b))
+  Tmax <- -delta_f0/log(p0)
+  T <- Tmax
+  
+  ## the simulated annealing loop
+  while(count < iter){
+    
+    #obtaining the testing solution x'
+    move <- sample(2:n, 2)
+    testsol <- swap(sol, move[1], move[2])
+    testfit <- mass_center(testsol)
+    
+    #checking if we replace x by x'
+    if(exp(-(testfit-fit)/T) > runif(1)){
+      sol <- testsol
+      fit <- testfit
+    }
+    #updating the best solution
+    if(testfit <= bestfit){
+      bestsol <- testsol
+      bestfit <- testfit
+      count <- 1
+    }else{
+      count <- count + 1
+    }
+    
+    #keeping record of evolution of fit, bestfit and temperature if eval=TRUE
+    if(eval){
+      evalfit <- c(evalfit, fit)
+      evalbest <- c(evalbest, bestfit)
+      temp <- c(temp, T)
+    }
+    
+    T <- alpha*T
+    
+  }
+  #returning the solution
+  if(eval)
+    return(list(sol=bestsol, fit=bestfit, evalfit=evalfit, evalbest=evalbest, temp=temp))
+  else
+    return(list(sol=bestsol, fit=bestfit))
+}
+
+
+
+set.seed(1313)
+test <- sa_circle(1:37, 
+                  iter = 1000, 
+                  alpha = 0.99, 
+                  p0 = 0.5, 
+                  eval = TRUE)
+
+test$fit
+
+
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+
 
 
 
 ### Example of how to write SA in r code without package 
 # https://rpubs.com/mstefan-rpubs/salesman
+
+## Create fake cities 
+# "not in" operator
+'%!in%' <- function(x,y)!('%in%'(x,y))
+# extend "LETTERS" function to run from "A" to "ZZ"
+MORELETTERS <- c(LETTERS, sapply(LETTERS, function(x) paste0(x, LETTERS)))
+# example
+MORELETTERS[20:30]
+
+## Put fake cities' coordinates in df 
+# function to simulate cities
+simCities <- function(n_cities = 5) {
+  cities <- matrix(runif(2*n_cities,-1,1),ncol=2)
+  rownames(cities) <- MORELETTERS[1:n_cities]
+  colnames(cities) <- c("x","y")
+  return(cities)
+}
+# example
+cities <- simCities(n = 10)
+cities 
+
+
+## plot cities 
+# function to plot cities
+plotCities <- function(cities, 
+                       main="",
+                       bg="white", 
+                       main_col="black",
+                       point_col="deepskyblue") {
+  par(bg=bg)
+  plot(cities, 
+       pch=16, cex=3,
+       col=point_col, 
+       ylim=c(-1,1.1), xlim=c(-1,1),
+       yaxt="n", xaxt="n",
+       ylab="", xlab="",
+       bty="n",
+       main=main, col.main=main_col)
+}
+
+# example
+plotCities(cities)
+
+
+## Function stat generates random routes that start and end with city A
+# function to generate a random route
+randomRoute <- function(cities) {
+  start <- rownames(cities)[1]
+  route <- sample(rownames(cities)[-1])
+  route <- c(start,route,start) # return back home
+  return(route)
+}
+
+# example
+route <- randomRoute(cities)
+route
+
+
+## Distances between cities..
+# function to compute adjacency matrix
+compAdjMat <- function(cities) return(as.matrix(dist(cities)))
+
+# example
+cities <- simCities(5)
+adjmat <- compAdjMat(cities)
+adjmat
+
+# function to compute the distance of a route
+distRoute <- function(adjmat, route) {
+  d <- 0
+  for(i in 2:nrow(adjmat)) {
+    d <- d + adjmat[route[i-1],route[i]]
+  }
+  return(d)
+}
+
+# example
+route1 <- c("A","C","E","B","D","A")
+distRoute(adjmat, route1)
+
+### simulated annealing 
+
+simAnneal <- function(cities, temp=1e4,
+                      cooling=5e-3, break_after=1e2,
+                      sleep=0.05) {
+  
+  # compute adjacency matrix
+  adjmat <- compAdjMat(cities)
+  
+  # start with random route
+  best_route <- randomRoute(cities)
+  min_d <- distRoute(adjmat,best_route)
+  
+  # variable tracking
+  track_temp <- c()
+  track_prob <- c()
+  track_dist <- c()
+  
+  # iterative loop
+  stable_count <- 0
+  while(stable_count < break_after) {
+    
+    # conduct swap
+    ik <- sort(sample(2:nrow(cities),2))
+    new_route <- swap(best_route, i=ik[1], k=ik[2])
+    new_d <- distRoute(adjmat,new_route)
+    
+    # probability of adjusting route
+    improvement <- min_d - new_d
+    p_adjust <- ifelse(improvement > 0, 1, exp(improvement/temp))
+    
+    # adjust route?
+    adjust <- ifelse(p_adjust >= runif(1,0,1), T, F)
+    
+    # if adjustment
+    if(adjust) {
+      best_route <- new_route
+      min_d <- new_d
+      stable_count <- 0
+      plotRoute(cities, best_route, min_d)
+      legend("topright", legend=round(temp,4), 
+             bg="transparent", 
+             bty="n",
+             text.col="black")
+      Sys.sleep(sleep)
+    } else {
+      stable_count <- stable_count+1
+    }
+    
+    # update on variable tracking
+    track_temp <- c(track_temp,temp)
+    track_prob <- c(track_prob,p_adjust)
+    track_dist <- c(track_dist,new_d)
+    
+    # cool down
+    temp <- temp*(1-cooling)
+    
+  } # end of iterative loop
+  
+  # return
+  return(list(distance = min_d,
+              route = best_route,
+              track_temp = track_temp,
+              track_prob = track_prob,
+              track_dist = track_dist)
+  )
+  
+  
+}
+# example
+simAnneal(cities)
 
 
 
@@ -293,11 +627,6 @@ SA_T1 <- Sys.time() # record end time
 
 
 # Matejka, Justin, and George Fitzmaurice. “Same stats, different graphs: Generating datasets with varied appearance and identical statistics through simulated annealing.” Proceedings of the 2017 CHI Conference on Human Factors in Computing Systems. ACM, 2017.
-
-### published info with GenSA package
-# https://journal.r-project.org/archive/2013/RJ-2013-002/RJ-2013-002.pdf
-  # information for how to use GenSA package
-
 
 
 # ?https://gist.github.com/robertness/e69127ed752ef78f78db
